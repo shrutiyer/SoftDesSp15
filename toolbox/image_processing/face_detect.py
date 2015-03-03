0c514d70768bc458f93ac91d095071f8850cc975
shrutiyer <shruti.iyer@students.olin.edu>
2015-03-02 20:23:39 -0500

    adding a .txt file

--- toolbox/image_processing/face_detect.py
+++ toolbox/image_processing/face_detect.py
@@ -1,4 +1,28 @@
 """ Experiment with face detection and image filtering using OpenCV """
 
-import cv2
 import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minSize=(20,20))
+for (x,y,w,h) in faces:
+    	cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255))
+ 	
+ 	# Display the resulting frame
+	cv2.imshow('frame',frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+    	break
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+face_cascade = cv2.CascadeClassifier('/home/shruti/Soft Des/6/haarcascade_frontalface_alt.xml')
