94edf322e6b36bdbf09c340a3199cd779af56f5f
shrutiyer <shruti.iyer@students.olin.edu>
2015-03-30 14:14:21 -0400

    inclass problem 1

--- inclass/oop_practice/problem1.py
+++ inclass/oop_practice/problem1.py
@@ -10,8 +10,9 @@ class Date(object):
             year: the year (represented as an integer)
             This method will not validate whether a given date is invalid
             (e.g. April 31, 2000) """
-        pass
-
+        self.month = month
+        self.day = day
+        self.year = year
 
     def is_before(self,other_date):
         """ Returns true if and only if self occurs before other_date
@@ -21,7 +22,15 @@ class Date(object):
             >>> d1.is_before(d2)
             True
         """
-        pass
+        if self.year < other_date.year:
+            return True
+        elif self.month < other_date.month and self.year == other_date.year:
+            return True
+        elif self.month == other_date.month and self.year == other_date.year:
+            return self.day < other_date.day
+        else:
+            return False
+
 
 
     def __str__(self):
@@ -31,12 +40,21 @@ class Date(object):
         >>> print Date(3,26,2015)
         March, 26th 2015
         """
-        pass
+        monthDict = {1 : "January", 2 : "February", 3 : "March",
+                     4 : "April", 5 : "May", 6 : "June",
+                     7 : "July", 8 : "August", 9 : "September",
+                     10 : "October", 11 : "November", 12 : "December"}
+
+        thDict= {0 : "th", 1 : "st", 2 : "nd",
+                 3 : "rd", 4 : "th", 5 : "th",
+                 6 : "th", 7 : "th", 8 : "th", 9 : "th"}
+        return "%s, %i%s %i" % (monthDict[self.month],self.day,
+                                thDict[self.day % 10], self.year)
 
     def increment_year(self):
         """ Modifies the Date object self so that it represents a date of the
             same month and day but for the following year. """
-        pass
+        self.year += 1
 
     def is_leap_year(self):
         """ Returns true if the year that this date falls in is a leap year
